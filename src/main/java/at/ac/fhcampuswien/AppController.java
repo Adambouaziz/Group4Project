97f06bdca4eb20984880f0f211dc320caeafdcdd
Adam Bouaziz <adam.bouaziz@stud.fh-campuswien.ac.at>
2022-03-05 12:21:26 +0100

    Second 5 articles

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -39,8 +39,14 @@ public class AppController {
         Article floridaMan = new Article("Franz Jospeh", "Florida man burned down house with spaghetti sauce");
         Article transformers = new Article("Shia Lebouf", "Cars are able to speak now...?");
         Article encanto = new Article("Mirabel Madrigal", "I blew out my family's special candle");
+        Article cyborgs = new Article("Chris Baker", "Why Cyborgs will endanger our species.");
+        Article running = new Article("Ham Middleton", "Is running really healthy?");
+        Article doomsday = new Article("Pam Jackson", "Bitcoin doomsday: Why it will fall to 0.");
+        Article invest = new Article("Wojak Trapper", "What you should invest all your money in RIGHT NOW!!");
+        Article chalamet = new Article("Kitty Kat", "Why is Timothée Chalamet so hot???");
 
-        List<Article> liste = Arrays.asList(bitcoinRave, deadCat, floridaMan, transformers, encanto);
+
+        List<Article> liste = Arrays.asList(bitcoinRave, deadCat, floridaMan, transformers, encanto, cyborgs, running, doomsday, invest, chalamet);
         return liste;
     }
 }
